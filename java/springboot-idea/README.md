1ce901c58292cd18abadaf8ef39e785a1bab02aa
Lei Xu <leixu@leixu-macpro16>
2022-01-02 00:04:39 +0800

    added java springboot-idea template

---
+++ java/springboot-idea/README.md
@@ -0,0 +1,12 @@
+# SmartIDE - Java SpringBoot Sample
+
+Java Springboot 示例应用
+
+启动命令
+
+```shell
+se start https://gitee.com/smartide/spring-boot-tutorial.git
+```
+
+ref:
+- https://www.jetbrains.com/help/idea/your-first-spring-application.html
---
+++ java/springboot-idea/src/main/java/com/example/springboottutorial/SpringBootTutorialApplication.java
@@ -0,0 +1,22 @@
+package com.example.springboottutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTutorialApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+}

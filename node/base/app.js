62f7b9164eeb1dac28c6faf73d0fdac563933df8
Lei Xu <leixu@leixu-macpro16>
2022-02-08 16:30:03 +0800

    添加node vscode模版

--- node/base/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static('public'));
-var routes = require("./api/routes");
-routes(app);
-
-module.exports = app;
--- node/base/test/helpers.js
+++
@@ -1,7 +0,0 @@
-var supertest = require('supertest');
-var chai = require('chai');
-var app = require('../server-test');
-
-global.app = app;
-global.expect = chai.expect;
-global.request = supertest(app);
